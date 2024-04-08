3b0d418801e82d9024b851eab7f11e22853d602b
Timothy Kageni <118418723+Timothyke@users.noreply.github.com>
2024-04-08 08:51:24 +0300

    Update Database .sql

--- Database .sql
+++ Database .sql
@@ -4,7 +4,7 @@ USE login_system;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL
+    password VARCHAR(50) NOT NULL
 );
 INSERT INTO users (username, password) VALUES
 ('user1', 'password1'),
